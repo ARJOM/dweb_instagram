154d93c8b87373546d2fa51ed79b9cfb197bab75
vmendes1331 <vmendes1331@gmail.com>
2018-07-22 23:09:23 -0300

    whattheflask

--- app/views.py
+++ app/views.py
@@ -4,8 +4,10 @@ import psycopg2
 import psycopg2.extras
 from flask import g, request, render_template
 from app import app
-from flask_sqlalchemy import SQLAlchemy
+'''from flask_sqlalchemy import SQLAlchemy'''
 '''from flask.ext.uploads import UploadSet, configure_uploads, IMAGES'''
+'''from werkzeug.utils import secure_filename'''
+from datetime import datetime
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Connect database
@@ -13,10 +15,14 @@ from flask_sqlalchemy import SQLAlchemy
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 '''
 photos = UploadSet('photos', IMAGES)
+
+app.config['UPLOADED_PHOTOS_DEST'] = 'static/img'
 configure_uploads(app, photos)
 '''
 conn = psycopg2.connect("dbname=instagram user=postgres password=flasknao host=127.0.0.1")
 cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+now = datetime.now()
 
 @app.route('/')
 def index():
@@ -45,30 +51,23 @@ def login():
         return render_template('login.html', error='usuario nao existe')
     return render_template('login.html')
 
+@app.route('/comment', methods=['GET', 'POST'])
+def comment():
+    if (request.method == 'POST'):
+        
+        cur.execute("INSERT INTO comment(id, date_c, comment, id_photo) VALUES (id, now, comment, id_photo)")
+        conn.commit()
+    return render_template('home.html')
+'''
 @app.route('/upload', methods=['POST'])
 def upload():
     file = request.files['inputFile']
     return file.filename
-'''
-@app.route('/feed', methods=['GET', 'POST'])
+
+@app.route('/upload', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST' and 'photo' in request.files:
         filename = photos.save(request.files['photo'])
-        rec = Photo(filename=filename, user=g.user.id)
-        rec.store()
-        flash("Photo saved.")
-        return redirect(url_for('show', id=rec.id))
         return filename
     return render_template('upload.html')
-
-@app.route('/comment', methods=['GET', 'POST'])
-def comment():
-    if (request.method == 'POST'):
-        id = request.form['id']
-        date_c = request.form['date_c']
-        comment = request.form['comment']
-        id_photo = request.form['id_photo']
-        cur.execute("INSERT INTO comment(id, date_c, comment, id_photo) VALUES (%s, %s, '%s', %s)" %(id, date_c, comment, id_photo))
-        conn.commit()
-    return render_template('home.html')
 '''
